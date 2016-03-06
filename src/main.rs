96cf6f6d9b3e8681abf68d004b53e13ecc59a61b
Derek Boone <boonesoftware@gmail.com>
2016-03-05 22:56:26 -0500

    Find project id for first project
    Created a generic `get` function that can be used for various GitLab API
    tasks. Moved private token to a global.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,57 @@
 extern crate curl;
+extern crate rustc_serialize;
 
-use std::env;
+use curl::{http, ErrCode};
+use curl::http::Response;
+use rustc_serialize::{json, Encodable, Decodable};
 use std::str::from_utf8;
-use curl::http;
 
-fn main() {
-	let token = match env::args().nth(1) {
-		Some(t) => t,
-		None => {
-			println!("expected private token");
-			return
-		},
-	};
+static SERVER : &'static str = "gitlab/";
+static API_PATH : &'static str = "api/v3";
+static TOKEN : &'static str = "<private-token>";
 
-	let resp = http::handle()
-		.get("gitlab/api/v3/projects/1")
-		.header("PRIVATE-TOKEN", &token)
-		.exec();
+fn get(path : &str) -> Result<Response, ErrCode> {
+	let command = SERVER.to_string() + API_PATH + path;
+
+	http::handle()
+		.get(command)
+		.header("PRIVATE-TOKEN", &TOKEN)
+		.exec()
+}
+
+// TODO - this should use /projects and iterate
+// through all of the projects searching for one
+// that matches the given name
+
+// TODO - this should have proper error handling
+fn get_project_id(project : &str) -> u32 {
+	#[derive(RustcEncodable, RustcDecodable)]
+	struct ProjectIdResponse { id : u32, }
+
+	let result = get("/projects/1");
 	
-	match resp {
-		Ok(r) => {
-			println!("code: {}", r.get_code());
-
-			let body = from_utf8(r.get_body());
+	match result {
+		Ok(response) => {
+			let body = from_utf8(response.get_body());
 			match body {
-				Ok(b) => println!("body:\n{}", b),
+				Ok(b) => {
+					let id_response : ProjectIdResponse = json::decode(&b).unwrap();
+					return id_response.id
+				},
 				Err(e) => {
 					println!("error: {}", e);
-					return
+					return 2^31
 				},
 			}
 		}
 		Err(e) => {
 			println!("error: {}", e);
-			return
+			return 2^31
 		},
 	}
+}
+
+fn main() {
+	let id0 = get_project_id("my-awesome-project");
+	println!("{}", id0);
 }

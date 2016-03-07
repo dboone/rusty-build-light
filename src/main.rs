a4a0acff5481f86481dfdc82765c5e21590ff588
Derek Boone <boonesoftware@gmail.com>
2016-03-06 22:18:49 -0500

    Get list of recently updated branches

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate chrono;
 extern crate curl;
 extern crate rustc_serialize;
 
+use chrono::{DateTime, UTC};
 use curl::{http, ErrCode};
 use curl::http::Response;
 use rustc_serialize::{json, Encodable, Decodable};
@@ -49,10 +51,56 @@ fn get_project_id(project : &str) -> Option<u32> {
 	}
 }
 
-fn main() {
-	let id0 = get_project_id("my-awesome-project");
-	println!("{}", id0.unwrap());
+fn get_recent_branches(project_id : u32, days : i64) -> Option<Vec<String>> {
+	#[derive(RustcEncodable, RustcDecodable)]
+	struct BranchResponse {
+		name : String,
+		commit : CommitResponse,
+	}
 
-	let id1 = get_project_id("my-lame-project");
-	println!("{}", id1.unwrap());
+	#[derive(RustcEncodable, RustcDecodable)]
+	struct CommitResponse {
+		committed_date : String,
+	}
+
+	let path = "/projects/".to_string() + &project_id.to_string() + "/repository/branches";
+	let result = get(&path);
+
+	match result {
+		Ok(response) => {
+			let body = from_utf8(response.get_body());
+			match body {
+				Ok(b) => {
+					let responses : Vec<BranchResponse> = json::decode(&b).unwrap();
+					let mut results : Vec<String>  = Vec::new();
+					for response in responses.iter() {
+						let committed_date = DateTime::parse_from_str(&response.commit.committed_date, "%Y-%m-%dT%H:%M:%S%.3f%z").unwrap();
+						let difference = UTC::now() - committed_date;
+						
+						if difference.num_days() < days {
+							results.push(response.name.clone());
+						}
+					}
+					Some(results)
+				},
+				Err(e) => { println!("error: {}", e); None },
+			}
+		}
+		Err(e) => {	println!("error: {}", e); None },
+	}
+}
+
+fn main() {
+	let project = "my-awesome-project";
+
+	println!("details for {}", project);
+
+	let id = get_project_id(project).unwrap();
+	println!("project id: {}", id);
+
+	print!("recent branches: ");
+	let branches = get_recent_branches(id, 10).unwrap();
+	for branch in branches {
+		print!("{}, ", branch);
+	}
 }

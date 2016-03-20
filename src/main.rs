f86c4e74db6be71d8a0daf12af4cb8a7476bf932
Derek Boone <boonesoftware@gmail.com>
2016-03-20 09:43:57 -0400

    Update whitespace

--- src/main.rs
+++ src/main.rs
@@ -35,14 +35,14 @@ fn main() {
     fn process_request(request: &mut Request) -> IronResult<Response> {
 		println!("Received request!");
 		let mut payload = String::new();
-        request.body.read_to_string(&mut payload).unwrap();
+		request.body.read_to_string(&mut payload).unwrap();
 		let build_message : BuildMessage = serde_json::from_str(&payload).unwrap();
 		println!("Project {} has new commit {} on branch {} with status {}",
 			build_message.repository.name,
 			build_message.commit.sha,
 			build_message.reference,
 			build_message.build_status);
-        Ok(Response::with((status::Ok, payload)))
+		Ok(Response::with((status::Ok, payload)))
     }
 
     Iron::new(process_request).http("192.168.0.105:3000").unwrap();
